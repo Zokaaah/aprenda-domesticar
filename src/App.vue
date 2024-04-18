357c2ed51e050eb2af3fc2a9c39bcbf80d16e1ac
Gabriel de Oliveira <gabriel.oliveira_012@hotmail.com>
2024-04-18 12:00:05 -0300

    update

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+
+import Banner from './components/Banner.vue'
+</script>
+
+<template>
+  <header>
+
+  </header>
+
+  <main>
+    <Banner/>
+
+  </main>
+</template>
+
+<style scoped>
+body{
+  background-color: #fff !important;
+}
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="bannerbkg">
+
+    <div class="banner">
+        <h1>Aprenda a<br/>domesticar seu pet</h1>
+        <button class="btnStart">Quero começar</button>
+    </div>
+
+  </div>
+  <b-container>
+
+  <div class="stats">
+    <div v-for="(item) in Stats" :key="item.id" class="icones" >
+                        <img  :src="item.icon" alt="">
+                        <br/>
+                        <h3>{{item.name}}</h3>
+                    </div>
+
+  </div>
+</b-container>
+</template>
+
+<script>
+export default {
+name:'Banner',
+
+data(){
+    return{
+        Stats:[
+                    {id:1, name:'Pagamento seguro', icon:'./images/pagseguro.png'},
+                    {id:2, name:'Acesso seguro', icon:'./images/acesseguro.png'},
+                    {id:3, name:'Garantia 7 dias', icon:'./images/garantia.png'},
+                ],
+    }
+}
+
+}
+</script>
+
+<style scoped>
+.banner{
+   justify-content: center;
+   align-items: center;
+   justify-items: center;
+   align-content: center;
+   margin:0 auto !important;
+   font-weight: bold !important;
+    
+}
+
+.bannerbkg{
+    background-image: url(../assets/banner.png); 
+    height: 780px;
+    width: 100%;
+    background-repeat: no-repeat;
+    background-size: cover;
+    display: flex;
+    text-align: center;
+
+}
+
+h1{
+    color: #fff;
+    font-size: 80px;
+    font-weight: bold !important;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    margin-top: -20px;
+}
+.btnStart{
+    background-color: #18AC39;
+    color: #fff;
+    font-weight: bold;
+    border-radius: 14px;
+    margin-top: 5%;
+    padding: 2%;
+    font-size: 32px;
+    border: none;
+    cursor: pointer;
+    transition: all 0.3s;
+    -webkit-transition: width 1s;
+    transition: width 1s;
+    -webkit-transition: all .5s;
+    -moz-transition: all .5s;
+    transition: all .5s;
+}
+.btnStart:hover{
+    transform: scale(1.1);
+
+}
+.stats{
+    background-color: #292a2c;
+    text-align: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+
+
+}
+.icones{
+    margin-top: 2%;
+    margin-bottom: 2%;
+
+    width: 35%;
+}
+
+h3{
+    font-weight: bold;
+    font-size: 1.2em;
+}
+
+
+
+</style>
